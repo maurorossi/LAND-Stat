b6228808a341089ce9180d237ddcb067a7d94872
Mauro Rossi <mauro.rossi76@gmail.com>
2016-12-29 18:24:39 +0100

    Update run.R

--- src/main/app-resources/job_template_landstat/run.R
+++ src/main/app-resources/job_template_landstat/run.R
@@ -13,26 +13,30 @@ myparam2 <- rciop.getparam("param2")
 # add a log message
 rciop.log("DEBUG", paste("I'm running a job with parameter values:", myparam1, myparam2, sep=" "))
 
-# read the inputs coming from stdin
-f <- file("stdin")
-open(f)
+# # read the inputs coming from stdin
+# f <- file("stdin")
+# open(f)
 
-while(length(input <- readLines(f, n=1)) > 0) {
+# while(length(input <- readLines(f, n=1)) > 0) {
   
-  rciop.log("INFO", paste("processing input:", input, sep=" "))
+#   rciop.log("INFO", paste("processing input:", input, sep=" "))
   
-  # copy the input to the process temporary folder TMPDIR
-  res <- rciop.copy(input, TMPDIR, uncompress=TRUE)
+#   # copy the input to the process temporary folder TMPDIR
+#   res <- rciop.copy(input, TMPDIR, uncompress=TRUE)
   
-  if (res$exit.code==0) local.url <- res$output
+#   if (res$exit.code==0) local.url <- res$output
   
-  mycsv <- read.csv(local.url)
+#   mycsv <- read.csv(local.url)
   
-  # do something with the downloaded csv here in TMPDIR/output
+#   # do something with the downloaded csv here in TMPDIR/output
   
-  # publish the any results done 
-  rciop.publish(paste(TMPDIR,"output", sep="/"), recursive=TRUE, metalink=FALSE)
+#   # publish the any results done 
+#   rciop.publish(paste(TMPDIR,"output", sep="/"), recursive=TRUE, metalink=FALSE)
  
-}
+# }
+data_random<-rnorm(10,mean=myparam2,sd=myparam1)
+data_random2<-rnorm(10,mean=myparam2,sd=myparam1)
+
+dt<-data.frame(x=data_random,y=data_random2)
 
 ##a<-hist(rnorm(1000))
